b941ccc4cf49521e32c78be35735d270cf4cd2f0
Rui Geng <rui.geng@navblue.aero>
2017-10-23 15:47:56 -0400

    add a DependencyProperty

--- WPFGesture/WPFGesture/MainWindow.xaml.cs
+++ WPFGesture/WPFGesture/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFGesture
 {
@@ -20,6 +7,25 @@ namespace WPFGesture
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty GestureTextProperty =
+            DependencyProperty.Register(
+                "GestureText",
+                typeof(string),
+                typeof(MainWindow),
+                new PropertyMetadata(string.Empty));
+
+        public string GestureText
+        {
+            get
+            {
+                return (string)GetValue(GestureTextProperty);
+            }
+            set
+            {
+                SetValue(GestureTextProperty, value);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
